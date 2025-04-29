4566b52efa52a1ac935def85e4e1b29721f1f2c7
Clippy95 <qpzezoqp15@gmail.com>
2025-04-29 19:37:56 +0300

    Add project files.

---
+++ dllmain.cpp
@@ -0,0 +1,123 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+#include "MinHook.h"
+#include <windows.h>
+#include <iostream>
+#include "Hooking.Patterns.h"
+#include "safetyhook.hpp"
+#pragma comment(lib, "libMinHook.x86.lib")
+#include "shared.h"
+typedef cvar_t*(*Cvar_GetT)(char* var_name, const char* var_value, int flags);
+Cvar_GetT Cvar_Get = (Cvar_GetT)0x004337F0;
+cvar_t* cg_fovscale;
+void codDLLhooks(HMODULE handle);
+
+typedef HMODULE(__cdecl* LoadsDLLsT)(const char* a1, FARPROC* a2, int a3);
+LoadsDLLsT originalLoadDLL = nullptr;
+
+typedef double(*CG_GetViewFovT)();
+CG_GetViewFovT CG_GetViewFov_og = nullptr;
+
+SafetyHookInline CG_GetViewFov_og_S{};
+
+void OpenConsole()
+{
+    AllocConsole();
+    freopen("CONOUT$", "w", stdout);
+    freopen("CONIN$", "r", stdin);
+    std::cout << "Console initialized.\n";
+    printf("hi");
+}
+
+double CG_GetViewFov_hook() {
+    double fov = CG_GetViewFov_og_S.call<double>();
+    if (cg_fovscale && cg_fovscale->value)
+        fov = fov * cg_fovscale->value;
+    return fov;
+
+}
+
+void CheckModule()
+{
+    HMODULE hMod = GetModuleHandle(L"uo_cgamex86.dll");
+    if (hMod)
+    {
+        std::cout << "uo_cgamex86.dll is attached at address: " << hMod << std::endl;
+        codDLLhooks(hMod);
+    }
+    else
+    {
+        std::cout << "uo_cgamex86.dll is NOT attached.\n";
+    }
+}
+
+HMODULE __cdecl hookCOD_dllLoad(const char* a1, FARPROC* a2, int a3) {
+    HMODULE result = originalLoadDLL(a1, a2, a3);
+    CheckModule();
+    printf("0x%X \n", (int)result);
+    return result;
+}
+
+void InitHook() {
+    cg_fovscale = Cvar_Get((char*)"cg_fovscale", "1.0", CVAR_ARCHIVE);
+    if (MH_Initialize() != MH_OK) {
+        //MessageBoxW(NULL, L"FAILED TO INITIALIZE", L"Error", MB_OK | MB_ICONERROR);
+        return;
+    }
+    MessageBoxW(NULL, L"FAILED TO ENABLE", L"Error", MB_OK | MB_ICONERROR);
+    if (MH_CreateHook((void**)0x454440, &hookCOD_dllLoad, (void**)&originalLoadDLL) != MH_OK) {
+        //MessageBoxW(NULL, L"FAILED TO HOOK", L"Error", MB_OK | MB_ICONERROR);
+        return;
+    }
+    if (MH_EnableHook(MH_ALL_HOOKS) != MH_OK) {
+        //MessageBoxW(NULL, L"FAILED TO ENABLE", L"Error", MB_OK | MB_ICONERROR);
+        return;
+    }
+
+    //MessageBoxW(NULL, L"FAILED TO ENABLE", L"Error", MB_OK | MB_ICONERROR);
+}
+
+void codDLLhooks(HMODULE handle) {
+    printf("run");
+    uintptr_t OFFSET = (uintptr_t)handle;
+    printf("HANDLE : 0x%p ADDR : 0x%p \n", handle, OFFSET + 0x2CC20);
+    CG_GetViewFov_og_S.reset();
+    CG_GetViewFov_og_S = safetyhook::create_inline(OFFSET + 0x2CC20, &CG_GetViewFov_hook);
+        //if (MH_CreateHook((void**)OFFSET + 0x2CC20, &CG_GetViewFov_hook, (void**)&CG_GetViewFov_og) != MH_OK) {
+        //    MessageBoxW(NULL, L"FAILED TO HOOK", L"Error", MB_OK | MB_ICONERROR);
+        //    return;
+        //}
+        
+        if (MH_EnableHook(CG_GetViewFov_og) != MH_OK) {
+            MessageBoxW(NULL, L"FAILED TO ENABLE", L"Error", MB_OK | MB_ICONERROR);
+            return;
+        }
+    
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        InitHook();
+        OpenConsole();
+        //CheckModule();
+        HMODULE moduleHandle;
+        // idk why but this makes it not DETATCH prematurely
+        GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, (LPCTSTR)DllMain, &moduleHandle);
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+        //FreeConsole();
+        //MH_Uninitialize();
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
---
+++ shared.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#define CVAR_ARCHIVE        1   // set to cause it to be saved to vars.rc
+// used for system variables, not for player
+// specific configurations
+#define CVAR_USERINFO       2   // sent to server on connect or change
+#define CVAR_SERVERINFO     4   // sent in response to front end requests
+#define CVAR_SYSTEMINFO     8   // these cvars will be duplicated on all clients
+#define CVAR_INIT           16  // don't allow change from console at all,
+// but can be set from the command line
+#define CVAR_LATCH          32  // will only change when C code next does
+// a Cvar_Get(), so it can't be changed
+// without proper initialization.  modified
+// will be set, even though the value hasn't
+// changed yet
+#define CVAR_ROM            64  // display only, cannot be set by user at all
+#define CVAR_USER_CREATED   128 // created by a set command
+#define CVAR_TEMP           256 // can be set even when cheats are disabled, but is not archived
+#define CVAR_CHEAT          512 // can not be changed if cheats are disabled
+#define CVAR_NORESTART      1024    // do not clear when a cvar_restart is issued
+#define CVAR_WOLFINFO       2048    // DHM - NERVE :: Like userinfo, but for wolf multiplayer info
+
+#define CVAR_UNSAFE         4096    // ydnar: unsafe system cvars (renderer, sound settings, anything that might cause a crash)
+#define CVAR_SERVERINFO_NOUPDATE        8192    // gordon: WONT automatically send this to clients, but server browsers will see it
+
+
+#define MAX_STRING_CHARS    1024    // max length of a string passed to Cmd_TokenizeString
+#define MAX_STRING_TOKENS   256     // max tokens resulting from Cmd_TokenizeString
+#define MAX_TOKEN_CHARS     1024    // max length of an individual token
+
+#define MAX_RELIABLE_COMMANDS 64
+
+#define MAX_INFO_STRING     1024
+#define MAX_INFO_KEY        1024
+#define MAX_INFO_VALUE      1024
+
+#define BIG_INFO_STRING     8192    // used for system info key only
+#define BIG_INFO_KEY        8192
+#define BIG_INFO_VALUE      8192
+
+#define MAX_NAME_LENGTH     36      // max length of a client name
+
+#define MAX_QPATH 64
+#define MAX_OSPATH 256
+
+enum qboolean
+{
+	qfalse = 0x0,
+	qtrue = 0x1,
+};
+typedef struct cvar_s
+{
+	char* name;
+	char* string;
+	char* resetString;
+	char* latchedString;
+	int flags;
+	qboolean modified;
+	int modificationCount;
+	float value;
+	int integer;
+	struct cvar_s* next;
+	struct cvar_s* hashNext;
+} cvar_t;
+typedef int cvarHandle_t;
+struct vmCvar_t
+{
+	cvarHandle_t handle;
+	int modificationCount;
+	float value;
+	int integer;
+	char string[256];
+};
